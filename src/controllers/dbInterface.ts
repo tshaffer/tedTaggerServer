1de39a8a60b036d197f3b6565ca3e823c4d6ecf4
Ted Shaffer <shaffer.family@gmail.com>
2023-10-21 05:45:10 -0700

    Add people from photos to tags collection.

--- src/controllers/dbInterface.ts
+++ src/controllers/dbInterface.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import {
   getMediaitemModel,
   getTagModel,
@@ -6,10 +8,13 @@ import {
   MediaItem,
   Tag,
 } from "../types";
+import { isArray } from 'lodash';
 
 export const getAllMediaItemsFromDb = async (): Promise<MediaItem[]> => {
 
   const mediaItemModel = getMediaitemModel();
+
+  const tagNames: Set<string> = new Set<string>();
 
   const mediaItems: MediaItem[] = [];
   // const documents: any = await (mediaItemModel as any).find().limit(100).exec();
@@ -18,8 +23,62 @@ export const getAllMediaItemsFromDb = async (): Promise<MediaItem[]> => {
     const mediaItem: MediaItem = document.toObject() as MediaItem;
     mediaItem.googleId = document.googleId.toString();
     mediaItems.push(mediaItem);
+
+    if (isArray(mediaItem.people)) {
+      // person: { _id, name }
+      mediaItem.people.forEach((person: any) => {
+        tagNames.add(person.name);
+      });
+    }
+
+  }
+
+  if (tagNames.size > 0) {
+    await (addTagsSetToDb(tagNames));
   }
   return mediaItems;
+}
+
+export const addTagsSetToDb = async (tagsSet: Set<string>): Promise<void> => {
+
+  const existingTags = await getAllTagsFromDb();
+  const existingTagNames: string[] = existingTags.map((aTag: Tag) => {
+    return aTag.label;
+  })
+  const existingTagsSet: Set<string> = new Set<string>(existingTagNames);
+
+  const tagsToAddToDb: Tag[] = [];
+
+  for (let tag of tagsSet) {
+    if (!existingTagsSet.has(tag)) {
+      tagsToAddToDb.push({
+        id: uuidv4(),
+        label: tag,
+      });
+    }
+  }
+
+  if (tagsToAddToDb.length > 0) {
+    const tagModel = getTagModel();
+    try {
+      return tagModel.collection.insertMany(tagsToAddToDb)
+        .then((retVal: any) => {
+          console.log('tags added successfully');
+          console.log(retVal);
+          return;
+        })
+        .catch((error: any) => {
+          console.log('db add error: ', error);
+          if (error.code === 11000) {
+            return;
+          } else {
+            debugger;
+          }
+        });
+    } catch (error: any) {
+      debugger;
+    }
+  }
 }
 
 export const getAllTagsFromDb = async (): Promise<Tag[]> => {
@@ -31,7 +90,7 @@ export const getAllTagsFromDb = async (): Promise<Tag[]> => {
   for (const document of documents) {
     const tag: Tag = document.toObject() as Tag;
     tag.id = document.id.toString();
-    tag.label =document.label.toString();
+    tag.label = document.label.toString();
     tags.push(tag);
   }
   return tags;
